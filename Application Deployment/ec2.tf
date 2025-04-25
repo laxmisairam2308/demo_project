745f1717c9859602afc7b6b259f8d057e555ea6f
laxmisairam2308 <108729203+laxmisairam2308@users.noreply.github.com>
2025-04-25 17:42:44 +0530

    Update ec2.tf

--- Application Deployment/ec2.tf
+++ Application Deployment/ec2.tf
@@ -7,15 +7,14 @@ resource "aws_instance" "server" {
 
   connection {
     type        = "ssh"
-    user        = "ubuntu"  # Replace with the appropriate username for your EC2 instance
-    private_key = file("/c/Users/Mathesh M/.ssh/id_rsa")  # Replace with the path to your private key
+    user        = "ubuntu"  
+    private_key = file("/home/ec2-user/.ssh/") 
     host        = self.public_ip
    }
 
-   # File provisioner to copy a file from local to the remote EC2 instance
    provisioner "file" {
-    source      = "../Flask Application/app.py"  # Replace with the path to your local file
-    destination = "/home/ubuntu/app.py"  # Replace with the path on the remote instance
+    source      = "../Flask Application/app.py" 
+    destination = "/home/ubuntu/app.py"  
    }
 
    provisioner "remote-exec" {
